240dcf39546ae9b958ee44c0772250e16662f02c
Mikhail <sikretmiha@gmail.com>
2026-03-09 20:23:36 +0200

    API + backend  added

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import fastf1
+
+# Creating web-app
+app = FastAPI()
+
+# Allowing cross adress gets
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_h2h_telemetry(year, track, session_name, driver1, driver2):
     fastf1.Cache.enable_cache('cache_folder') 
     
-    print(f"Загружаю сессию: {year} {track} {session_name}...")
-    # Optimization: weather and race director turned off
     session = fastf1.get_session(year, track, session_name)
     session.load(telemetry=True, weather=False, messages=False) 
     
-    # Fastest laps
     lap_d1 = session.laps.pick_drivers(driver1).pick_fastest()
     lap_d2 = session.laps.pick_drivers(driver2).pick_fastest()
     
-    # Getting the telemetry
     tel_d1 = lap_d1.get_car_data().add_distance()
     tel_d2 = lap_d2.get_car_data().add_distance()
     
-    # Packing into dictionary (future JSON)
-    # Method .tolist() setting Pandas table to python-list
-    data = {
+    return {
         driver1: {
             "distance": tel_d1['Distance'].tolist(),
             "speed": tel_d1['Speed'].tolist(),
@@ -34,15 +42,10 @@ def get_h2h_telemetry(year, track, session_name, driver1, driver2):
             "gear": tel_d2['nGear'].tolist()
         }
     }
-    
-    return data
 
-# Test
-if __name__ == "__main__":
-    # LEC vs RUS
-    result = get_h2h_telemetry(2026, 1, 'Q', 'ANT', 'RUS')
-    
-    print("\n--- DATA IS HERE ---")
-    print(f"Количество точек телеметрии у ANT: {len(result['ANT']['distance'])}")
-    print(f"Первые 5 значений скорости ANT: {result['ANT']['speed'][:5]}")
-    print(f"Первые 5 значений скорости RUS: {result['RUS']['speed'][:5]}")
+# Creating route (API endpoint)
+@app.get("/api/telemetry")
+def get_telemetry(year: int, track: int, session: str, driver1: str, driver2: str):
+    print(f"Пришел запрос: {year} {track} {session} | {driver1} vs {driver2}")
+    # Return result. FastAPI creates JSON from it.
+    return get_h2h_telemetry(year, track, session, driver1, driver2)
